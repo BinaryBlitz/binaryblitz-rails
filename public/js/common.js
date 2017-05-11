1145610531fbef7ccdca51b7941c67cbf721a5bf
Dan Kim <itsdanya@gmail.com>
2017-05-12 00:38:24 +0300

    Clean up scripts

--- public/js/common.js
+++ public/js/common.js
@@ -1,27 +1,27 @@
 $(document).ready(function() {
-  
-  //Burger menu
+
+  // Burger menu
   $('.burger-wrap').click(function(){
     $(this).children().toggleClass('active');
     $('.nav ul').toggleClass('active');
     return false;
   });
-  
+
   // Calc inputs
-  
+
   $('.calc-select').click(function() {
-    
+
     var radio = false;
-    
+
     if( $(this).find('input').attr('type') == "radio" ) {
       radio = true;
     }
-    
+
     if( radio ) {
       $(this).parent().find('.calc-select').removeClass('active');
       $(this).addClass('active');
     }
-    
+
     else {
       if( $(this).hasClass('active') ) {
         $(this).removeClass('active');
@@ -30,19 +30,19 @@ $(document).ready(function() {
         $(this).addClass('active');
       }
     }
-      
-    
+
+
     $(this).find('input').prop('checked', true);
   });
-  
+
   $('.calc-slider').on('afterChange', function(slick, currentSlide){
-    
+
     if( currentSlide.currentSlide > 0 ) {
       $('.calc-prev').addClass('active');
     } else {
       $('.calc-prev').removeClass('active');
     }
-    
+
     if( currentSlide.currentSlide == $('.calc-item').size() -1 ) {
       $('.calc-submit').addClass('active');
       $('.calc-next').removeClass('active');
@@ -51,19 +51,19 @@ $(document).ready(function() {
       $('.calc-next').addClass('active');
     }
   });
-  
+
   $('.calc-next').click(function() {
     $('.calc-slider').slick('slickNext');
     return false;
   });
-  
+
   $('.calc-prev').click(function() {
     $('.calc-slider').slick('slickPrev');
     return false;
   });
-  
+
   // Calc slider
-  
+
   $('.calc-slider').slick({
     speed: 300,
     dots: true,
@@ -72,16 +72,11 @@ $(document).ready(function() {
     swipe: false,
     infinite: false
   });
-  
+
   // Fancybox
-  
+
   $('.fancybox').fancybox({
     padding: 0
   });
-  
+
 });
-
-
-
-
-
