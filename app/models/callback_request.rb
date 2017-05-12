7d72db35fb1a77f728eb895d180a2051ffa131bf
SumLare <mrhard95@gmail.com>
2017-05-12 15:00:35 +0300

    Send email after callback request is created

--- app/models/callback_request.rb
+++ app/models/callback_request.rb
@@ -3,4 +3,12 @@ class CallbackRequest < ApplicationRecord
 
   validates :name, presence: true
   validates :phone_number, presence: true
+
+  after_create :send_email
+
+  private
+
+  def send_email
+    CallbackRequestMailer.new_callback_request(self).deliver
+  end
 end
