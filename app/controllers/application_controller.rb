d7aae43c0eb068c5ee18e9ca40f5fff416a5d8d7
SumLare <mrhard95@gmail.com>
2017-05-13 18:34:19 +0300

    Routes localication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
   protect_from_forgery with: :exception
+
+  protected
+
+  def set_locale
+    I18n.locale = params[:locale] || :ru
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
 
-  get 'contact', to: 'callback_requests#new'
-  get 'projects', to: 'pages#projects'
-  get 'calculator', to: 'pages#calculator'
-  get 'chisto', to: 'pages#chisto'
+  scope '(:locale)', locale: /en|ru/ do
+    get 'contact', to: 'callback_requests#new'
+    get 'projects', to: 'pages#projects'
+    get 'calculator', to: 'pages#calculator'
+    get 'chisto', to: 'pages#chisto'
 
-  resources :callback_requests, only: [:create]
+    resources :callback_requests, only: [:create]
+  end
 end
