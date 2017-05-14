a37f197918f98f18e2e730dcac97237f5635b35f
SumLare <mrhard95@gmail.com>
2017-05-14 14:51:32 +0300

    Change recipient mail to email in secrets

--- app/mailers/callback_request_mailer.rb
+++ app/mailers/callback_request_mailer.rb
@@ -2,7 +2,7 @@ class CallbackRequestMailer < ApplicationMailer
   def new_callback_request(callback_request)
     @callback_request = callback_request
     mail(
-      to: Rails.application.secrets.recipient_mail,
+      to: Rails.application.secrets.recipient_email,
       subject: "Заказ на обратный звонок №#{@callback_request.id}"
     )
   end
