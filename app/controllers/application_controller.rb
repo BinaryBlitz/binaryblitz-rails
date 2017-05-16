f60fcfa1861f34300e04f3b2cf955e3125d664d5
SumLare <mrhard95@gmail.com>
2017-05-16 16:34:08 +0300

    Routes localization (#4)
    * Routes localication
    
    * Fix loading assets in views
    
    * Use default locale in set locale method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
   protect_from_forgery with: :exception
+
+  protected
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 end
