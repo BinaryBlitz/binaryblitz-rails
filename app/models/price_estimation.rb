c3ff5c01ee28184f8ae922c7ef525fbc3a7ebad5
SumLare <mrhard95@gmail.com>
2017-05-18 14:15:06 +0300

    Allow blank fields in price estimation

--- app/models/price_estimation.rb
+++ app/models/price_estimation.rb
@@ -1,8 +1,10 @@
 class PriceEstimation < ApplicationRecord
+  include Phonable
+
   validates :name, presence: true
-  validates :phone_number, presence: true
-  validates :communication_method, inclusion: { in: %w(phone email) }
-  validates :specification_stage, inclusion: { in: %w(complete half-complete none) }
+  validates :phone_number, phone: true, presence: true
+  validates :communication_method, inclusion: { in: %w(phone email) }, allow_blank: true
+  validates :specification_stage, inclusion: { in: %w(complete half-complete none) }, allow_blank: true
   validate :valid_platforms
   validate :valid_registration_methods
   validate :valid_notification_methods
@@ -10,18 +12,21 @@ class PriceEstimation < ApplicationRecord
   private
 
   def valid_platforms
+    return unless platforms
     unless (platforms - %w(ios android)).empty?
       errors.add(:base, 'platform is not from the list')
     end
   end
 
   def valid_registration_methods
+    return unless registration_methods
     unless (registration_methods - %w(phone socials password)).empty?
       errors.add(:base, 'registration method is not from the list')
     end
   end
 
   def valid_notification_methods
+    return unless notification_methods
     unless (notification_methods - %w(push email sms)).empty?
       errors.add(:base, 'notification method is not from the list')
     end
