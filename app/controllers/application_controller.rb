7584a8f690e1657cb5709d8f285a70a749e46db2
Dan Kim <itsdanya@gmail.com>
2017-06-30 22:39:48 +0300

    Extract locale from TLD

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,17 @@ class ApplicationController < ActionController::Base
   protected
 
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    I18n.locale = extract_locale_from_tld || I18n.default_locale
+  end
+
+  # Extract locale from top-level domain:
+  # binaryblitz.ru => :ru, binaryblitz.xyz => :en, else return nil
+  def extract_locale_from_tld
+    tld = request.host.split('.').last
+
+    case tld
+    when 'ru' then :ru
+    when 'xyz' then :en
+    end
   end
 end
